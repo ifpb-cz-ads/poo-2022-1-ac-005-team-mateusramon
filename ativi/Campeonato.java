38ffd7e1cbd75383b49844340b7b97718af469ca
Morgan-Sa <71098129+Morgan-Sa@users.noreply.github.com>
2022-06-20 23:43:42 -0300

    Delete ativi directory

--- ativi/Campeonato.java
+++
@@ -1,6 +0,0 @@
-/*QUESTAO 05 */
-package ativi;
-
-public class Campeonato {
-    public String timeX;
-}
--- ativi/supermercado.java
+++
@@ -1,9 +0,0 @@
-/*QUESTAO 04 */
-package ativi;
-
-public class supermercado {
-    public String Lampada;
-}
-    
-
-    
